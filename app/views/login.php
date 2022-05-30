a29bf6e22065c518d722f5befbbbdcdc239899e3
Elbouhmydy Abdelmoughit <elbouhmydyabdelmoughit@gmail.com>
2022-05-30 19:46:41 +0000

    create page login but not finished

--- app/views/login.php
+++ app/views/login.php
@@ -14,11 +14,35 @@
   <title>Login</title>
 </head>
 
-<body>
+<body id="body">
+  <nav class="navbar navbar-expand-lg bg-white shadow" id="NAVBAR">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <span class="fw-bold fs-2 d-flex">YOU <span id="text_logo">CAR</span></span>
+      </a>
+      <button class="navbar-toggler" id="icons_menu" type="button" data-bs-toggle="collapse"
+        data-bs-target="#navbar_menu" aria-controls="navbar_menu" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbar_menu">
+        <ul class="navbar-nav mx-auto me-0 mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="index.php" id="btn_login">Home</a>
+          </li>
+      </div>
+    </div>
+  </nav>
+  <!------section Login page----->
+  <div class="container" id="container">
+    <div class="contant p-2 col-10 d-flex border border-1 border-danger mt-4" id="contant">
+      <div class="col-6 border border-1 border-info">
 
-
-
-
+      </div>
+      <div class="col-6 border border-1 border-prmary" id="img_body_login_page">
+        <img src="assets/img/img_body_login.jpg" class="img-responsive w-50" id="img_login" alt="Image Body Login Page">
+      </div>
+    </div>
+  </div>
 
   <script src="assets/js/script.js"></script>
   <script src="assets/js/sweetalert2.js"></script>
