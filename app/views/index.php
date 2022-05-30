9b27360020550cfc4c06662e387618d95d7b9098
Elbouhmydy Abdelmoughit <elbouhmydyabdelmoughit@gmail.com>
2022-05-30 10:07:17 +0000

    create responsive for landing page

--- app/views/index.php
+++ app/views/index.php
@@ -51,15 +51,16 @@
   <div class="container-fliud bg-white my-4">
     <div class="d-flex col-12">
       <!-----------section1 landing page text and button----------------->
-      <div class="col-6" id="section1_landing">
+      <div class="col-12 col-lg-6 " id="section1_landing">
         <div class="contact_section1">
           <h1 class="text-wrap" id="text_primary_landing">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h1>
           <button type="button" class="btn" id="btn_section1"> Continue</button>
         </div>
       </div>
       <!-----------section2 landing page image----------------->
-      <div class="col-6" id="section2_landing">
-        <img src="assets/img/img-body.png" class="img-responsive" id="img_body_landing_page" alt="image landing page">
+      <div class="col" id="section2_landing">
+        <img src="assets/img/img-body.png" class="img-responsive d-md-block d-none" id="img_body_landing_page"
+          alt="image landing page">
       </div>
     </div>
   </div>
